56b6e58d8e49b154cfe84f7d017b304ac8200cff
Michael Dono <michael@michaeldono.com>
2018-04-28 14:53:16 +0100

    Changed direction shown on screen.

--- controller.py
+++ controller.py
@@ -3,23 +3,26 @@ import radio
 class Controller:
     def __init__(self):
         radio.on()
-        radio.config(channel=88)
+        radio.config(channel=10)
         while (True):
             gesture = accelerometer.current_gesture()
             #self.sendRadio(gesture)
             if (gesture == "up"):
-                display.show(Image.ARROW_N)
-                radio.send("FORWARD")
-            elif (gesture == "down"):
                 display.show(Image.ARROW_S)
-                radio.send("BACKWARD")
+                radio.send('FORWARD')
+            elif (gesture == "down"):
+                display.show(Image.ARROW_N)
+                radio.send('BACKWARD')
             elif (gesture == "left"):
                 display.show(Image.ARROW_W)
-                radio.send("LEFT")
+                radio.send('LEFT')
             elif (gesture == "right"):
                 display.show(Image.ARROW_E)
-                radio.send("RIGHT")
-            #sleep(500)
+                radio.send('RIGHT')
+            else:
+                display.show(Image.DIAMOND)
+                radio.send('error')
+            sleep(20)
     def getRotation(self):
         return accelerometer.get_x(), accelerometer.get_y(), accelerometer.get_z()
 c = Controller()
